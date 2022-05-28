80676b52a5a7076ef1353f5a23a726b22b7eaf2c
Ripunjay Narula <ripunjay30narula@gmail.com>
2022-05-29 01:57:57 +0530

    ft: button func

--- src/App.js
+++ src/App.js
@@ -1,10 +1,34 @@
-import MyComponent from "./MyComponent";
-import { React, useEffect, useState } from "react";
-import { Navbar, Nav, Container, Row, Col, Card } from "react-bootstrap";
+import { React, useState } from "react";
+import {
+  Navbar,
+  Nav,
+  Container,
+  Row,
+  Col,
+  Card,
+  Button,
+} from "react-bootstrap";
 import logo from "./logo.svg";
 import "./App.css";
-
+function getRandomInt(max) {
+  return Math.floor(Math.random() * max);
+}
 function App() {
+  const [items, setItems] = useState([]);
+
+  const fetchData = () => {
+    fetch("https://jsonplaceholder.typicode.com/comments")
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        setItems(data);
+      });
+  };
+
+  var m = getRandomInt(100);
+  var n = 100 + getRandomInt(500);
+
   return (
     <div className="App">
       <Navbar bg="light" variant="light" sticky="top">
@@ -30,12 +54,28 @@ function App() {
 
       <Row style={{ marginTop: "10%" }} className="justify-content-center">
         <Col md={{ span: 6, offset: 0 }}>
-          <input style={{ width: "50vw" }} />
+          <input placeholder="Enter Query" style={{ width: "50vw" }} />
         </Col>
       </Row>
-      <Row style={{ marginTop: "5%", marginBottom:"1%" }} className="justify-content-center">
-        <Card style={{maxWidth:"70vw"}}>
-          <Card.Body><MyComponent /></Card.Body>
+      <Button onClick={fetchData} style={{ marginTop: "1%" }} variant="primary">
+        Execute
+      </Button>
+      <Row
+        style={{ marginTop: "5%", marginBottom: "1%" }}
+        className="justify-content-center"
+      >
+        <Card style={{ maxWidth: "70vw" }}>
+          <Card.Body>
+            <ul>
+              {items.map((item,i) => (
+                <p key={i}>
+                  
+                  {/* {(item.id>m && item.id<n)?{{item.name} {item.price}}:""} */}
+                  {item.name} {item.price}
+                </p>
+              ))}
+            </ul>
+          </Card.Body>
         </Card>
       </Row>
     </div>
